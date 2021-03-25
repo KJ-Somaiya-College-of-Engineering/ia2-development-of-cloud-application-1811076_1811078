fe6a1864fcf7e105f6d1a9cc4a2df506c78ddf09
Rajan Gaul <rajan.gaul@somaiya.edu>
2021-03-26 00:17:54 +0530

    Fix build errors

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Header from "./Header";
-import Footer from "./Footer";
+import Footer from "./Footer/Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
