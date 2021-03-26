4045b1c9099b8972852e9a79f1ecdef436334116
Rajan Gaul <rajan.gaul@somaiya.edu>
2021-03-26 10:30:51 +0530

    Update jest version

---
+++ build/precache-manifest.abfa6e05459e90e2706d96183b6ce602.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbfb2b7d1ee4eb96e8cd00800a5bd11c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d73e22b5b0551308d1f0",
+    "url": "/static/js/2.e8ba9aa3.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/static/js/2.e8ba9aa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e70385c245aa7124038d",
+    "url": "/static/js/main.818df08a.chunk.js"
+  },
+  {
+    "revision": "6b5468143bd4a0505d16",
+    "url": "/static/js/runtime-main.a3b46ef1.js"
+  }
+]);
