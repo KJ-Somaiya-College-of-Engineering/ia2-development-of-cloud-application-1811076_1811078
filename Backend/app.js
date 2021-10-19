a4919722ec20319f4ad8e1625e374b2c7c6fb752
Rajan Gaul <rajan.gaul@somaiya.edu>
2021-10-19 23:48:01 +0530

    Change directory structure

---
+++ Backend/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:3080"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({
+  extended: true
+}));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Notes on cloud!!" });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
