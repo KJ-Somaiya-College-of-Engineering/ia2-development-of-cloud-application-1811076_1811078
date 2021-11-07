5200825dd273fd884464184168a9efa5454379c2
Rajan Gaul <rajan.gaul@somaiya.edu>
2021-11-07 23:21:40 +0530

    Make POST request to save the note to database

--- Frontend/src/components/Note/CreateArea.js
+++ Frontend/src/components/Note/CreateArea.js
@@ -1,4 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import axios from 'axios';
+import {API_BASE_ROUTE, REQUEST_HEADERS} from '../../config/serverConfig';
+import UserProvider from "../../context/user.provider";
 
 const BLANK_NOTE = {
   title: "",
@@ -8,6 +11,7 @@ const BLANK_NOTE = {
 const CreateArea = (props) => {
   const [note, setNote] = useState(BLANK_NOTE);
   const [errorVisibility, setErrorVisibility] = useState(false);
+  const userData = useContext(UserProvider.context);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -27,14 +31,30 @@ const CreateArea = (props) => {
     }
   }
 
-  const submitNote = (event) => {
+  const addNoteToDB = async(note) =>{
+    const newNote = {
+      email: userData.email,
+      noteTitle: note.title,
+      noteContent: note.content,
+      timestamp: new Date(),
+    }
+    const response = await axios.post(`${API_BASE_ROUTE}/note/new`, newNote, {headers:{'x-access-token':userData.accessToken, ...REQUEST_HEADERS}});
+  }
+
+
+  const submitNote = async(event) => {
     event.preventDefault();
     if(note.title.trim() === "" || note.content.trim() === ""){
       setErrorVisibility(true);
       return;
     }
-    props.onAdd(note);
-    setNote(BLANK_NOTE);
+    try{
+      await addNoteToDB(note);
+      setNote(BLANK_NOTE);
+      props.refreshNotes(userData.email, userData.accessToken);
+    }catch(err){
+      console.log(err.response);
+    }
   }
 
   return (
