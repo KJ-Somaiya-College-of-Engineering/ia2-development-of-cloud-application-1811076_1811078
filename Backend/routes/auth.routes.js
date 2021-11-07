64f4f916fb588cf88eec8a10b271c594fe9a93f9
Rajan Gaul <rajan.gaul@somaiya.edu>
2021-11-07 23:15:29 +0530

    Fix auth route headers

--- Backend/routes/auth.routes.js
+++ Backend/routes/auth.routes.js
@@ -3,10 +3,10 @@ const controller = require("../controllers/auth.controller");
 
 module.exports = function (app) {
     app.use(function (req, res, next) {
-        res.header(
-            "Access-Control-Allow-Headers",
-            "x-access-token, Origin, Content-Type, Accept"
-        );
+        res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
         next();
     });
 
