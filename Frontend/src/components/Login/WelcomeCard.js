0094d1474445574021a3196b1f59e9134a16f06f
Rajan Gaul <rajan.gaul@somaiya.edu>
2021-11-07 23:18:32 +0530

    Add register and login page links

--- Frontend/src/components/Login/WelcomeCard.js
+++ Frontend/src/components/Login/WelcomeCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 
 export default function WelcomeCard() {
     return (
@@ -8,6 +9,12 @@ export default function WelcomeCard() {
                     <h1>Welcome Back!</h1>
                     <p className="signInMessage">Sign In to continue using</p>
                     <span className="notesOnCloudSignIn">Notes On Cloud</span>
+                    <div className="loginFooter">
+                        <p className="signInMessage">Don't have an account?</p>
+                        <Link to="/register" className="authFooterLink">
+                            <span >Register</span>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
--- Frontend/src/components/Register/InfoCard.js
+++ Frontend/src/components/Register/InfoCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 
 export default function InfoCard() {
     return (
@@ -9,6 +10,12 @@ export default function InfoCard() {
                     <p className="registerMessage">Create new account with</p>
                     <p> your email to start using</p>
                     <p className="notesOnCloudRegister">Notes On Cloud</p>
+                    <div className="registerFooter">
+                        <p className="signInMessage">Already have an account?</p>
+                        <Link to="/login">
+                            <span className="authFooterLink">Login</span>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
