d2e01f5efbfaa4a07e62a764f4c091b696e8888c
Rajan Gaul <rajan.gaul@somaiya.edu>
2021-11-08 10:06:09 +0530

    Update more CORS Origins

--- Backend/routes/auth.routes.js
+++ Backend/routes/auth.routes.js
@@ -3,7 +3,7 @@ const controller = require("../controllers/auth.controller");
 
 module.exports = function (app) {
     app.use(function (req, res, next) {
-        res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+        res.header("Access-Control-Allow-Origin", "https://notesoncloud.netlify.app");
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
